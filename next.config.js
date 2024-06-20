70da8f00e388ce72aa5247d47f4d3d534965be42
edwardpringle <edward@pringle.vu>
2024-06-20 11:19:45 +0100

    nextconfig path to env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { config } = require('dotenv');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { path: `${process.env.ENVIRONMENT}`};
 
 module.exports = nextConfig;
