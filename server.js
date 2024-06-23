e817b284486e5c5005bb860b82019af6feaceaeb
edwardpringle <edward@pringle.vu>
2024-06-23 20:52:31 +0100

    return to original server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.prepare().then(() => {
       // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
-      console.log (`The parsedURL is ${parsedUrl}`)
+
       if (pathname === '/a') {
         await app.render(req, res, '/a', query)
       } else if (pathname === '/b') {
@@ -37,7 +37,7 @@ app.prepare().then(() => {
       process.exit(1)
     })
     .listen(port, () => {
-      console.log(`> Ready on https://${hostname}:${port}`)
+      console.log(`> Ready on http://${hostname}:${port}`)
     })
 })
 
