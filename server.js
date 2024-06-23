cd40c66f672385139cc01c98ecd52c596637a165
edwardpringle <edward@pringle.vu>
2024-06-23 20:03:13 +0100

    console log change 3

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.prepare().then(() => {
       process.exit(1)
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}, /n The URL parsed was: ${parsedUrl}`)
+      console.log(`> Ready on http://${hostname}:${port}. The URL parsed was: ${parsedUrl}`)
     })
 })
