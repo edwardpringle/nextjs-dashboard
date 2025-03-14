b37c2c29321ddd41aa129175bc4e854c79892268
edwardpringle <edward@pringle.vu>
2025-03-14 08:45:51 +0000

    changes to server.js to .ts, package.json and tsconfig

--- server.ts
+++ server.ts
@@ -1,21 +1,35 @@
-import { createServer } from 'node:http';
-//const { createServer } = require('node:http')
-import { parse } from 'node:url';
-//const { parse } = require('url')
+import { createServer } from 'http'
+import { parse } from 'url'
 import next from 'next'
-//const next = require('next')
-
  
 //testing the .cpanel.yml file to push to web
 
+const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+
+//const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = process.env.PORT || 3000
+//const port = process.env.PORT || 3000
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
+//const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+   
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
+
+
+/* app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
       // Be sure to pass `true` as the second argument to `url.parse`.
@@ -51,3 +65,4 @@ app.prepare().then(() => {
 //`> Ready on http://${hostname}:${port}`
 // `> Server listening at http://${hostname}:${port} as ${
 //      dev ? 'development' : process.env.NODE_ENV}`
+*/
