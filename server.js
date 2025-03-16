930f5dc1925eba76625772039a75fc18a696effe
edwardpringle <edward@pringle.vu>
2025-03-16 20:49:55 +0000

    rebuild with new custom server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
@@ -63,7 +63,7 @@ app.prepare().then(() => {
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
-*/
+/// Old code block out above this line
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
@@ -99,4 +99,27 @@ process.exit(1)
 .listen(port, () => {
 console.log(`> Ready on http://${hostname}:${port}`)
 })
+})
+*/
+///New Code:
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
 })
