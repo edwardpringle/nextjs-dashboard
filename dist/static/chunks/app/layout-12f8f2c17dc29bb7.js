405f78b11989f4c25c413fc219a57e88a8990ea9
edwardpringle <edward@pringle.vu>
2025-03-18 15:05:13 +0000

    build dashboard

--- dist/static/chunks/app/layout-12f8f2c17dc29bb7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1820:()=>{},3352:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1820,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[525,441,684,358],()=>s(3352)),_N_E=e.O()}]);
